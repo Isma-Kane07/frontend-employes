d1587a609ebf01839add516a808c48bdb574beff
Isma-Kane07 <ismakane07@gmail.com>
2025-08-25 17:42:02 +0000

    styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./*.html", // Assurez-vous d'ajouter tous les chemins pertinents
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
